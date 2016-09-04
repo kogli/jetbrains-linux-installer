8028dd6b1992e080b7ed5619f2d4d75c76d95c0e
kogli <koglimail@gmail.com>
2016-09-04 19:55:17 +0200

    Installer creates destination directory if doesn't exist

--- install-globally.sh
+++ install-globally.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-dest="/usr/local/bin/jetbrains-get"
+dest="/usr/local/bin"
+destFile="$dest/jetbrains-get"
 source="./jetbrains-get.sh"
 
 if [ $(id -u) -ne 0 ]; then
 	SUDO='sudo'
 fi
-
-$($SUDO cp $source $dest)
-$($SUDO chmod +x $dest)
-$($SUDO chown root:root $dest)
+$($SUDO mkdir -p $dest)
+$($SUDO cp $source $destFile)
+$($SUDO chmod +x $destFile)
+$($SUDO chown root:root $destFile)
 
 echo "jetbrains-get installed"
